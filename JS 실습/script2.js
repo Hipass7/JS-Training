b1a4c73f4da6d7f62968ec83e59b7d45cd929daf
Hipass7 <64785177+Hipass7@users.noreply.github.com>
2020-07-13 14:18:27 +0900

    Add files via upload

---
+++ JS 실습/script2.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    var hw = document.getElementById('hw');
+        hw.addEventListener('click', function(){
+            alert('Hello world');
+        })
+        hw.addEventListener('click', function(){
+            alert('World Hello');
+        })
+    }
